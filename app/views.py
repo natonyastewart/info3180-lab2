d5d92377c5bf34b53cdb3539ece0d206c4d4fca8
natonyastewart <natonyastewart15@gmail.com>
2025-02-23 19:36:46 -0500

    Added profile page

--- app/views.py
+++ app/views.py
@@ -1,10 +1,29 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
 # Routing for your application.
 ###
+
+def format_date_joined(date):
+    return date.strftime("%B, %Y")
+
+@app.route('/profile')
+def profile():
+    profile_info = {
+        "full_name": "Natonya Stewart",
+        "username": "natonyastewart",
+        "location": "Kingston, Jamaica",
+        "date_joined": format_date_joined(datetime.date(2025, 2, 23)),
+        "bio": "Passionate software developer with a love for technology, problem-solving, and innovation. Experienced in Python, Flask, and Web Development. When I'm not coding, you can find me exploring cybersecurity trends, playing video games, or enjoying a good day doing absolutely nothing.",
+        "posts": 55,
+        "followers": 450,
+        "following": 170,
+        "profile_image": "profile.jpg"
+    }
+    return render_template('profile.html', profile=profile_info)
 
 @app.route('/')
 def home():
